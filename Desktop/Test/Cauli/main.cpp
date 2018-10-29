41f482e4df173ad3e52d7650b3fd186eae9e0959
phamv <phamv@Tablezz.com>
2018-10-29 23:12:37 +0700

    update

--- Desktop/Test/Cauli/main.cpp
+++ Desktop/Test/Cauli/main.cpp
@@ -9,19 +9,20 @@ using namespace std;
 
 cauliflower flower;
 
-//xampleflower flower;
 float g_x = 0.0;
+float g_y = 0.0;
 float g_z = 0.0;
 float g_x_origin = 0.0;
 bool g_is_rotate = false;
-float g_angle = 0.0;
-float delta_angle = 0.0;
+float g_angle1 = 0.0;
+float delta_angle1 = 0.0;
+
 void display()
 {
-    glClearColor(255, 255, 255, 0); // Set background color to ưhite and opaque
+    glClearColor(255, 255, 255, 0); // Set background color to white and opaque
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);         // Clear the color buffer (background)
     glLoadIdentity();
-    gluLookAt(g_x, 1.0, g_z, 0, 0, 0, 0, 1, 0);
+    gluLookAt(g_x, g_y, g_z, 0, 0, 0, 0, 1, 0);
     glPushMatrix();
     flower.turtle3D();
     glPopMatrix();
@@ -44,14 +45,13 @@ void init()
     GLfloat diff_use [] = {0.0, 1.0, 0.0, 1.0};
     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff_use);
 
-    GLfloat specular[] = {0, 255, 255, 1.0};
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+//    GLfloat specular[] = {0, 255, 255, 1.0};
+//    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
 
     GLfloat shininess = 30.0f;
     glMateriali(GL_FRONT, GL_SHININESS, shininess);
-
-    g_x = DIS_CAM_TO_MODE*sin(g_angle);
-    g_z = DIS_CAM_TO_MODE*cos(g_angle);
+    g_x = DIS_CAM_TO_MODE*sin(g_angle1);
+    g_z = DIS_CAM_TO_MODE*cos(g_angle1);
 }
 void MouseButton(int type_button, int state, int x, int y)
 {
@@ -65,7 +65,7 @@ void MouseButton(int type_button, int state, int x, int y)
         else
         {
            g_is_rotate = false;
-           g_angle += delta_angle;
+           g_angle1 += delta_angle1;
            g_x_origin = -1;
         }
     }
@@ -74,11 +74,55 @@ void MouseButton(int type_button, int state, int x, int y)
 {
     if(g_is_rotate == true)
     {
-        delta_angle = (x - g_x_origin)*0.005f;
-        g_x = DIS_CAM_TO_MODE*sin(g_angle + delta_angle);
-        g_z = DIS_CAM_TO_MODE*cos(g_angle + delta_angle);
+        delta_angle1 = (x - g_x_origin)*0.005f;
+        g_x = DIS_CAM_TO_MODE*sin(g_angle1 + delta_angle1);
+        g_z = DIS_CAM_TO_MODE*cos(g_angle1 + delta_angle1);
     }
 }
+
+void onSpecialKeyDown(int key, int x, int y)
+{
+     switch(key)
+    {
+        case GLUT_KEY_UP:
+        {
+            g_z -= 0.2;
+            break;
+        }
+
+        case GLUT_KEY_DOWN:
+        {
+            g_z += 0.2;
+            break;
+        }
+        case GLUT_KEY_LEFT:
+        {
+            g_x -= 0.2;
+            break;
+        }
+        case GLUT_KEY_RIGHT:
+        {
+            g_x += 0.2;
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+void onSpecialKeyUp(int key, int x, int y)
+{
+     switch(key)
+    {
+        case GLUT_KEY_LEFT:
+            break;
+        case GLUT_KEY_RIGHT:
+            break;
+        default:
+            break;
+    }
+}
+
 void ReShape(int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -86,7 +130,6 @@ void ReShape(int width, int height)
     glLoadIdentity();
     float ratio = (float)width / (float)height;
     gluPerspective(45.0, ratio, 1, 100);
-    // glOrtho(-10.0, 10.0, -10.0, 10.0, 10.0, -10.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -98,7 +141,7 @@ int main(int argc, char **argv)
     {
         flower.generate();
     }
-    //flower.getSentence();
+
     glutInit(&argc, argv);                 // Initialize GLUT
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(600, 600);
@@ -107,9 +150,10 @@ int main(int argc, char **argv)
     init();
     glutDisplayFunc(display);              // Register display callback handler for window re-paint
     glutReshapeFunc(ReShape);
-
     glutMouseFunc(MouseButton);
     glutMotionFunc(MouseMove);
+    glutSpecialFunc(onSpecialKeyDown);
+    glutSpecialUpFunc(onSpecialKeyUp);
     glutMainLoop();                       // Enter the event-processing loop
     return 0;
 }
